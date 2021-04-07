74588cfa8c97311beff466a89c4c221bedebdb2f
Alyssa Smith <alyssa.dev.smith@gmail.com>
2021-04-08 07:11:24 +1000

    fix subpackages

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name="precisionapi",
-    packages=["precisionapi"],
+    packages=find_packages(),
     version="0.1.0",
     install_requires=["requests","attrs"],
 )
